697eb5be23cbf21ff5c0e1d6c2d6319f0a7a61e8
ifirat <ibrahim_firat@hotmail.com>
2019-09-11 22:09:48 +0200

    13. hafta odevler -Ibrahim

---
+++ odev 1 - staircase .py
@@ -0,0 +1,5 @@
+n = int(input("lutfen bir sayi giriniz :"))
+for i in range(1,n+1):
+    print(("#"*i).rjust(n), end="\n")
+
+
---
+++ odev 2 - between two sets .py
@@ -0,0 +1,12 @@
+arr = [3, 4]
+brr = [24, 48]
+list_first = []
+list_result = []
+
+for i in range(arr[-1],brr[0]+1):
+    list_first += [i for x in arr if i%x==0]
+    list_first += [i for y in brr if y%i==0]
+list_result = set([i for i in list_first if list_first.count(i) == len(arr)+len(brr)])
+
+print(len(list_result))
+
