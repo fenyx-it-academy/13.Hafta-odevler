ce35de69375d15eee2df3c76cc6fdb2549e81647
adnngrkly <itengineernetherlands@gmail.com>
2019-09-20 01:58:58 +0200

    3. hafta odevindeki hata duzeltildi

--- odev3.py
+++ odev3.py
@@ -3,7 +3,7 @@ def inputs():
     input2=int(input("lutfen bolmek istediginiz sayiyi giriniz"))
     liste=[]
     for i in range(input1):
-        number=int(input("Lutfen {} sayiyi giriniz").format(i+1))
+        number=int(input("Lutfen {} sayiyi giriniz".format(i+1)))
         liste.append(number)
     liste.insert(0,input2)
     return liste
@@ -31,11 +31,10 @@ def check():
     return liste1
 def consuquence():
     liste=check()
-    print(liste)
     input2=liste[0]
     liste.remove(input2)
     counter=0
-    for i in range(len(liste)):
+    for i in range(len(liste)-1):
         value=liste[i][0]+liste[i][1]
         if value%input2==0:
             counter+=1
